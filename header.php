a57a6da57af151902abd17190b89e352306f506a
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-10 21:44:55 -0800

    Add header script

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+  <h1>PHP Demo Website</h1>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Shop</a>
+      </li>
+    </ul>
+  </nav>
+</header>
