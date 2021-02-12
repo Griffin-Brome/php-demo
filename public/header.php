ee7063fcec0f3f2f17058a0483aac0da2d2013e6
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-11 20:12:46 -0800

    Add descriptions, reorganize directories

---
+++ public/header.php
@@ -0,0 +1,23 @@
+<!--
+Name: header.php
+Author: Griffin Brome
+License: MIT
+Description: Website header, should appear on all user faces pages.
+ -->
+
+<header>
+  <h1>PHP Demo Website</h1>
+  <nav>
+    <ul>
+      <li>
+        <a href="/index.php">Home</a>
+      </li>
+      <li>
+        <a href="/shop.php">Shop</a>
+      </li>
+      <li>
+        <a href="/admin.php">Admin</a>
+      </li>
+    </ul>
+  </nav>
+</header>
---
+++ public/shop.php
@@ -0,0 +1,17 @@
+<!--
+Name: shop.php
+Author: Griffin Brome
+License: MIT
+Description: Shop page, shows site inventory
+ -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Shop</title>
+  </head>
+  <body>
+    <?php include "header.php" ?>
+    <h2>Shop</h2>
+  </body>
+</html>
