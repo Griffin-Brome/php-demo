4cafa44bc223bacebb3d1057b14c4de807a81634
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-12 14:51:12 -0800

    Fix SQL syntax

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,17 @@
  * Description:
  */
 
-DROP TABLE IF EXISTS admin
-DROP TABLE IF EXISTS user
-DROP TABLE IF EXISTS cart
-DROP TABLE IF EXISTS item
-DROP TABLE IF EXISTS in_cart
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS item;
 
 CREATE TABLE admin (
   uname VARCHAR NOT NULL,
   pword CHAR(32) NOT NULL, --MD5 password hash
   PRIMARY KEY(uname)
-)
+);
 
 CREATE TABLE item (
   name VARCHAR NOT NULL,
   cost INT,
   details VARCHAR
-)
+);
