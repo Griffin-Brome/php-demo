793bcd7a30dc1429309135d347592b6e4938199e
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-11 20:03:04 -0800

    Add description comments, restructure php files

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!--
+Name: index.php
+Author: Griffin Brome
+License: MIT
+Description: Main landing page for website
+ -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Main Page</title>
+  </head>
+  <body>
+    <?php include "header.php"?>
+    <h2>Home</h2>
+  </body>
+</html> 
---
+++ php/new_item.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Name: new_item.php
+ * Author: Griffin Brome
+ * License: MIT
+ * Description: Receives a POST request containing the fields of an item to insert into the 'item'
+ * database table
+ */
+?>
