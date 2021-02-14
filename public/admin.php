5172ec2758e30e927818b680e6c8e82302f6b18d
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-13 22:54:13 -0800

    Move new item form labels to proper spots

--- public/admin.php
+++ public/admin.php
@@ -3,32 +3,34 @@ Name: admin.php
 Author: Griffin Brome
 License: MIT
 Description: Administrator portal.
- -->
+-->
 
 <!DOCTYPE html>
+<?php 
+    include "../php/header.php"
+?>
+
 <html>
   <head>
     <title>Admin Portal</title>
   </head>
 
   <body>
-    <?php include "../php/header.php"?>
 
     <h2>Administrator</h2>
-
     <h3>Add Item</h3>
-    <form action="../php/new_item.php" method="GET">
+    <form action="../php/new_item.php" method="POST">
       <fieldset>
         <legend>New Item</legend>
 
-        <label for="new-name">Item name:</label>
-        <input type="text" id="new-name" name="new-name">
+        <label for="new_name">Item name:</label>
+        <input type="text" id="new_name" name="new_name">
 
-        <label for="new-cost">Item cost:</label>
-        <input type="number" id="new-cost" name="new-cost" min="0" step="0.01">
+        <label for="new_cost">Item cost:</label>
+        <input type="number" id="new_cost" name="new_cost" min="0" step="0.01">
 
-        <label for="new-details">Item details:</label>
-        <textarea id="new-details" name="new-details"></textarea>
+        <label for="new_details">Item details:</label>
+        <textarea id="new_details" name="new_details"></textarea>
 
         <input type="submit" name="submit">
 
