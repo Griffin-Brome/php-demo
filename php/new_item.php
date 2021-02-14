0fdb9eb59b9378339f5b25875eb4b3119e942ef1
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-13 22:53:20 -0800

    Implement insert item for admin page

--- php/new_item.php
+++ php/new_item.php
@@ -6,4 +6,21 @@
  * Description: Receives a POST request containing the fields of an item to insert into the 'item'
  * database table
  */
+
+include("db.php");
+
+// Get contents of POST request
+if (! $_POST) {
+    die("Error: no data in POST request.");
+} else {
+    try {
+        $name = $_POST['new_name'];
+        $cost = $_POST['new_cost'];
+        $details = $_POST['new_details'];
+        createItem($name, $cost, $details);
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+    header('Location: ../public/admin.php'); // Redirect back to admin page
+}
 ?>
