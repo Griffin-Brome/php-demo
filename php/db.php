b31bb52723adfcfcbd8fc0f9b1d77da4154b2b7e
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-14 17:14:09 -0800

    Add 'get all items' function to db.php

--- php/db.php
+++ php/db.php
@@ -78,7 +78,17 @@ function deleteItem($name) {
 function getItem() {}
 
 /**
- * TODO
+ * @return an array (numeric/associative) of all rows from the item table
  */
-function getAllItems() {}
+function getAllItems() {
+    $con = getConnection();
+    $rst = $con->query("SELECT (name, cost, details) FROM item");
+    $rows = $rst->fetch_all();
+
+    if ($con->errno) {
+        die($con->error);
+    }
+    
+    return $rows;
+}
 ?>
