b8e4a327de8e5a0e9d4650581178359f96b16be3
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-02-16 22:00:05 -0800

    Lint PHP code with prettier-php
    https://loilo.github.io/prettier-php-playground/

--- php/delete_item.php
+++ php/delete_item.php
@@ -3,17 +3,17 @@
  * Name: delete_item.php
  * Author: Griffin Brome
  * License: MIT
- * Description:
+ * Description: Delete a row in the 'item' database
  */
 
-include("db.php");
+include "db.php";
 
 // Get contents of POST request
-if (! $_POST) {
+if (!$_POST) {
     die("Error: no data in POST request.");
 } else {
-    $name = $_POST['delete_name'];
+    $name = $_POST["delete_name"];
     deleteItem($name);
-    header('Location: ../public/admin.php'); // Redirect back to admin page
+    header("Location: ../public/admin.php"); // Redirect back to admin page
 }
 ?>
--- php/new_item.php
+++ php/new_item.php
@@ -6,16 +6,16 @@
  * Description: Receives a POST request containing the fields of an item to insert into the 'item' database table
  */
 
-include("db.php");
+include "db.php";
 
 // Get contents of POST request
-if (! $_POST) {
+if (!$_POST) {
     die("Error: no data in POST request.");
 } else {
-    $name = $_POST['new_name'];
-    $cost = $_POST['new_cost'];
-    $details = $_POST['new_details'];
+    $name = $_POST["new_name"];
+    $cost = $_POST["new_cost"];
+    $details = $_POST["new_details"];
     createItem($name, $cost, $details);
-    header('Location: ../public/admin.php'); // Redirect back to admin page
+    header("Location: ../public/admin.php"); // Redirect back to admin page
 }
 ?>
